c5f04ccfacf571803216be6ab2366de2e67afbab
Mehtmet ONAR <mhmtonar1@gmail.com>
2024-07-31 12:28:15 +0000

    string builder add

---
+++ doc/app/basic-app-17-spr3/src/main/java/com/example/basicapp17spr3/BasicApp17Spr3Application.java
@@ -0,0 +1,13 @@
+package com.example.basicapp17spr3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApp17Spr3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApp17Spr3Application.class, args);
+	}
+
+}
---
+++ doc/app/basic-app-17-spr3/src/test/java/com/example/basicapp17spr3/BasicApp17Spr3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicapp17spr3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApp17Spr3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
