67c491ad71ad909b1ce3612c5f1bfeb96f4bb96d
Mehtmet ONAR <mhmtonar1@gmail.com>
2024-08-01 05:21:32 +0000

    h2 db kaldırıldı

--- src/main/java/io/mhmtonrn/spc/SpcApplication.java
+++ src/main/java/io/mhmtonrn/spc/SpcApplication.java
@@ -2,12 +2,15 @@ package io.mhmtonrn.spc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SpcApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpcApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SpcApplication.class, args);
+    }
 
 }
